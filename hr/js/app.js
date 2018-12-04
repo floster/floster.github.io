60b80b5b01dc0ea89293f515a5f23884bbe6c4e8
valera.osadchuk <valera.osadchuk@gmail.com>
2018-12-04 21:52:48 +0200

    new version of Hardova Rosa (v1.1)

--- hr/js/app.js
+++ hr/js/app.js
@@ -1,3 +1,45 @@
 $(function(){
-	console.log('jQuery is working!');
+  var saveButtons              = $('#editPersonalInfoForm .save-buttons');
+	var editButtons              = $('#editPersonalInfoForm .edit-buttons');
+  var buttonEditData           = $('#cabinetEditPersonalInfo');
+  var buttonChangePassword     = $('#cabinetChangePassword');
+  var buttonCancelEdit         = $('#cabinetEditPersonalInfoCancel');
+  var buttonSavePassword       = $('#cabinetChangePasswordSave');
+  var buttonCancelPasswordEdit = $('#cabinetChangePasswordCancel');
+
+  var editPersonalInfoForm = $('#editPersonalInfoForm');
+  var changePasswordForm   = $('#changePasswordForm');
+
+  buttonEditData.on('click', function(){
+    editPersonalInfoForm.find('input').prop('disabled', false);
+    saveButtons.show();
+    editButtons.hide();
+  });
+
+  buttonCancelEdit.on('click', function(){
+    editPersonalInfoForm.find('input').prop('disabled', true);
+    saveButtons.hide();
+    editButtons.show();
+  });
+
+  buttonChangePassword.on('click', function(){
+    changePasswordForm.toggle('fast');
+  });
+  buttonCancelPasswordEdit.on('click', function(){
+    changePasswordForm.hide('fast');
+  });
+});
+
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      position = $(window).scrollTop();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
 });
